bd35e07e2ebce7c08905c56e08c2bba56e76b1d4
Nicolas  Jonason <nicolasjonason@Nicolass-MacBook-Pro.local>
2022-03-29 09:07:39 +0200

    update audioplayer layout

---
+++ src/App.jsx
@@ -0,0 +1,121 @@
+import './App.css';
+import React from 'react';
+import CustomAudioPlayer from './CustomAudioPlayer';
+
+const schemes = [
+  "sax_partial",
+  "scratch_nof0",
+  // "sax_pretrained_only",
+  "scratch_nof0_free_reverb",
+  "sax_whole",
+  "swc_nosax_partial",
+  "scratch",
+  "swc_nosax_whole",
+  "scratch_free_reverb"
+]
+
+
+const flutes = [
+  "9MKeGursT48_part0-00000-of-00001",
+  "be1jJCH32OU_part0-00000-of-00001",
+  "GY6_fvCO-qM_part0-00000-of-00001",
+  "eHbxLcoLWYY_part0-00000-of-00001"]
+
+const saxophones = [
+  "A8Q-O5KB7kw_part0-00000-of-00001",
+  "igCT4G1M1Xo_part0-00000-of-00001",
+  "ZQRZmdIda6s_part0-00000-of-00001",
+  "uPFmNhqERog_part0-00000-of-00001"]
+
+const trombones = [
+  "2bYLzLsIWME_part0-00000-of-00001",
+  "_W2VM3uinZA_part0-00000-of-00001",
+  "LBZ1EvU5bYY_part0-00000-of-00001",
+  "wmyWpgA3IOM_part0-00000-of-00001",]
+
+const instruments = {
+  "Flute": flutes,
+  "Saxophone": saxophones,
+  "Trombone": trombones
+}
+
+const durations = ["4", "8", "16", "32", "64", "128", "256"]
+
+const demo_approaches = ["whole", "partial"]
+const demo_durations = ["4", "8", "16", "32"]
+
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Neural Music Instrument Cloning - Web Supplement</h1>
+      <div >
+        <h2>Plot data</h2>
+        <h3>Target <CustomAudioPlayer controls src={process.env.PUBLIC_URL + "/audio/" + "unseen target.wav"} ></CustomAudioPlayer></h3>
+        <table style={{ width: "100vw" }}>
+          <thead>
+            <tr>
+              <th>approach / duration</th>
+              {durations.map(duration => <th>{duration}</th>)}
+            </tr>
+
+            <tr>
+              <td>training data excerpt</td>
+              {durations.map(duration =>
+                <td key={duration}>
+                  <CustomAudioPlayer src={process.env.PUBLIC_URL + "/audio/plot_data_final/" + "training_data" + "/d=" + duration + "_training_data.wav"} ></CustomAudioPlayer>
+                </td>)}
+            </tr>
+          </thead>
+          <tbody>
+            {schemes.map(scheme =>
+              <tr key={scheme}>
+                <td>{scheme}</td>
+                {durations.map(duration =>
+                  <td key={duration} >
+                    <CustomAudioPlayer controls src={process.env.PUBLIC_URL + "/audio/plot_data_final/" + scheme + "/d=" + duration + "_unseen_estimate.wav"}></CustomAudioPlayer>
+                  </td>)}
+              </tr>)}
+          </tbody>
+        </table>
+      </div>
+      {
+        Object.keys(instruments).map(instrument =>
+          <div key={instrument}>
+            <h3>{instrument}</h3>
+            {instruments[instrument].map(recording_name =>
+              <div>
+                <h3 key={recording_name}>{recording_name}</h3>
+                <h3>Target: <CustomAudioPlayer controls src={process.env.PUBLIC_URL + "/audio/demos/" + instrument + "/" + recording_name + "/" + "unseen_target.wav"}></CustomAudioPlayer></h3>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>approach / duration</th>
+                      {demo_durations.map(duration => <th>{duration}</th>)}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {demo_approaches.map(scheme =>
+                      <tr key={scheme}>
+                        <td>{scheme}</td>
+                        {demo_durations.map(duration =>
+                          <td key={duration} >
+                            <CustomAudioPlayer controls src={process.env.PUBLIC_URL + "/audio/demos/" + instrument + "/" + recording_name + "/" + scheme + "/d=" + duration + "_unseen_estimate.wav"}></CustomAudioPlayer>
+                          </td>)}
+                      </tr>)}
+                  </tbody>
+                </table>
+              </div>
+            )}
+          </div>
+        )
+      }
+
+    </div >
+
+  );
+}
+
+export default App;
---
+++ src/CustomAudioPlayer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css';
+
+const CustomAudioPlayer = ({ src }) => {
+    return <AudioPlayer showJumpControls={false} src={src} showSkipControls={false} customAdditionalControls={[RHAP_UI.CURRENT_TIME,
+    <div>/</div>,
+    RHAP_UI.DURATION]} customControlsSection={[RHAP_UI.MAIN_CONTROLS, RHAP_UI.ADDITIONAL_CONTROLS,]}
+
+        customProgressBarSection={
+            [
+                RHAP_UI.PROGRESS_BAR,
+            ]}
+
+    ></ AudioPlayer>
+}
+
+export default CustomAudioPlayer
