dc4f7ec939972359d83b7bde9e054119a913fed5
Nicolas  Jonason <nicolasjonason@Nicolass-MacBook-Pro.local>
2022-04-20 19:06:55 +0200

    add preprint notice

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => (
     <div>
         <h2>Home</h2>
-        This page is the web supplement for the paper <a href={process.env.PUBLIC_URL + "/Neural_Instrument_Cloning (32).pdf"}>Neural music instrument cloning from few samples</a> by Nicolas Jonason and Bob L.T. Sturm, submitted for review at DAFX22.
+        This page is the web supplement for the paper <a href={process.env.PUBLIC_URL + "/Neural_Instrument_Cloning_PREPRINT.pdf"}>Neural music instrument cloning from few samples</a> by Nicolas Jonason and Bob L.T. Sturm, submitted for review at DAFX22.
         <br></br>
         For more information, please visit the <a href="https://github.com/erl-j/neural-instrument-cloning">github</a> page.
     </div>
